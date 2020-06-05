13bb85093780769dd793c5ac6b93d8a78cbbc4c2
Sulhan Ageel <sulhan.ageel@gmail.com>
2020-06-05 04:11:32 +0000

    Revert to last successful build

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <AppHeader />
       <AppContainer />
-      <Map />
     </div>
   );
 }
--- src/components/Map.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Map() {
-
-    return <div id='map' style='width: 400px; height: 300px;'></div>
-}
-  
-export default Map
